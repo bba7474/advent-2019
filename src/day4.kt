3b756f29c040a84062e771ad8d2561416828c733
Ben Ashby <bashby91@gmail.com>
2019-12-05 00:02:19 +0000

    Day 4 Part 4

--- src/day4.kt
+++ src/day4.kt
@@ -1,8 +1,10 @@
 fun main() {
     val potentialPasswordsCount = IntRange(153517, 630395)
+        .asSequence()
         .map { p -> toOverlappingDigits(p) }
         .filter { p -> hasMatchingPair(p) }
         .filter { p -> isIncreasing(p) }
+        .filter { p -> matchingPairsNotPartOfLargerMatchingGroup(p) }
         .count()
 
     println("There are... $potentialPasswordsCount potential passwords")
@@ -14,6 +16,25 @@ fun hasMatchingPair(pairsOfDigits: List<Pair<Int, Int>>): Boolean {
 
 fun isIncreasing(pairsOfDigits: List<Pair<Int, Int>>): Boolean {
     return pairsOfDigits.all { pair -> pair.first <= pair.second }
+}
+
+fun matchingPairsNotPartOfLargerMatchingGroup(pairsOfDigits: List<Pair<Int, Int>>): Boolean {
+    for ((index, value) in pairsOfDigits.withIndex()) {
+        if (value.first == value.second) {
+            val previousMatch = if (index == 0) false else pairIsMatchingPairOfInt(pairsOfDigits.get(index - 1), value.first)
+            val nextMatch = if (index == pairsOfDigits.size - 1) false else pairIsMatchingPairOfInt(pairsOfDigits.get(index + 1), value.first)
+
+            if (!previousMatch && !nextMatch) {
+                return true
+            }
+        }
+    }
+
+    return false
+}
+
+fun pairIsMatchingPairOfInt(pair: Pair<Int, Int>, intToMatch: Int): Boolean {
+    return pair.first == intToMatch && pair.first == pair.second
 }
 
 fun toOverlappingDigits(input: Int): List<Pair<Int, Int>> {
